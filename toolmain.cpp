b7ac4f2d3e42979f1d4068ad712311a20fc3cdd4
wanhaipeng <529057232@qq.com>
2018-12-26 01:55:03 +0800

    add opencv lib and test

--- toolmain.cpp
+++ toolmain.cpp
@@ -1,11 +1,15 @@
 #include "toolmain.h"
 #include "ui_toolmain.h"
+#include <opencv2/opencv.hpp>
 
 ToolMain::ToolMain(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::ToolMain)
 {
     ui->setupUi(this);
+    cv::Mat inputimg = cv::imread("***/lena.jpg");
+    cv::imshow("hp_test", inputimg);
+    cv::waitKey(0);
 }
 
 ToolMain::~ToolMain()
