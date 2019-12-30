b290181bd47552b0debebcbc2577d6541a9aab4a
wanhaipeng <529057232@qq.com>
2019-12-30 10:32:18 +0800

    update CV_Tool

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "toolmain.h"
 #include <QApplication>
 
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
     QApplication a(argc, argv);
     ToolMain w;
--- toolmain.cpp
+++ toolmain.cpp
@@ -7,7 +7,7 @@ ToolMain::ToolMain(QWidget *parent) :
     ui(new Ui::ToolMain)
 {
     ui->setupUi(this);
-    cv::Mat inputimg = cv::imread("xxx/lena.jpg");
+    cv::Mat inputimg = cv::imread("/xxxxxxx/CV_Tool/lena.jpg");
     cv::imshow("test", inputimg);
     cv::waitKey(0);
 }
